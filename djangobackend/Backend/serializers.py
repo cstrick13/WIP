d0e5829da45f7b7774b7fd8279e2753f4b4b0982
KennethBores <kennethbores@gmail.com>
2024-09-28 14:27:39 -0500

    Made views and serializers for db models

--- djangobackend/Backend/serializers.py
+++ djangobackend/Backend/serializers.py
@@ -1,7 +1,35 @@
 from rest_framework import serializers
-from Backend.models import (Test)
+from .models import Test, User, Connection, Post, Reply
 
 class TestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Test
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'f_name', 'l_name', 'bio']
+
+class ConnectionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Connection
+        fields = ['follower','followed', 'timestamp']
+        
+    def validate(self, data):
+        if data['follower'] == data['followed']:
+            raise serializers.ValidationError("A user cannot follow themselves")
+        return data
+    
+class PostSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Post
+        fields = ['user', 'content', 'media_type', 'timestamp', 'likes']
+        
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['post', 'user', 'content', 'timestamp']
+        

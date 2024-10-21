e400ba13f08b1e4b189bc37ce5722b5b66f56ad4
cstrick13 <corystrickland13@gmail.com>
2024-10-21 14:45:15 -0500

    QUick Push

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SplashscreenComponent } from './splashscreen/splashscreen.component';
 import { authGuard } from './guards/auth.guard';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 const routes: Routes = [  
   { path: 'splash', component: SplashscreenComponent, canActivate: [authGuard]},
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'profile-page', component: ProfilePageComponent},
   { path: '', redirectTo: '/splash', pathMatch: 'full' },];
 
 @NgModule({
